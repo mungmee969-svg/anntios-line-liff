71ac708e6a09f147dd3c446dd87ce3b331d87442
anntios <mungmee969@gmail.com>
2026-05-07 17:54:42 +0700

    Update layout.tsx

--- src/app/backoffice/layout.tsx
+++ src/app/backoffice/layout.tsx
@@ -1,7 +1,20 @@
+"use client";
+
+import { usePathname } from "next/navigation";
 import { BackofficeGuard } from "@/src/components/backoffice/BackofficeGuard";
 import { BackofficeShell } from "@/src/components/backoffice/BackofficeShell";
 
-export default function BackofficeLayout({ children }: { children: React.ReactNode }) {
+export default function BackofficeLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const pathname = usePathname();
+
+  if (pathname === "/backoffice/login") {
+    return <>{children}</>;
+  }
+
   return (
     <BackofficeGuard>
       <BackofficeShell>{children}</BackofficeShell>
